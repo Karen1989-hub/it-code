6630dce98e50ecdf3660c62a788024e1c0a4140d
Karen1989-hub <aroyan.89@inbox.ru>
2022-01-18 23:31:00 +0400

    first commit

---
+++ app/Http/Controllers/AdminFrontPagesController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminFrontPagesController extends Controller
+{
+    public function getAdminLoginPage(){
+        
+    }
+}
